c3f33da4b4d4ba78428b844ee4f1211fed37fdf2
Elias Milian <00092822@uca.edu.sv>
2023-12-02 10:58:37 -0600

    solucion

---
+++ models/modelo_establecimiento.js
@@ -0,0 +1,20 @@
+const Mongoose = require('mongoose');
+const schema = Mongoose.Schema;
+
+const estabSchema = new schema({
+    title: { type: String, trim: true },
+    description: { type: String, trim: true },
+    priceRange: { type: String, trim: true },
+    category: { type: String, trim: true},
+    ubicacion: {
+        lat: { type: Number },
+        lng: { type: Number }
+      }
+    
+      
+    }); 
+
+const Establecimientos = Mongoose.model('Establecimientos', estabSchema);
+module.exports = {
+    Establecimientos
+};
